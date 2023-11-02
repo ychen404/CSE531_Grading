e03fbf95339347491b718ae90ae054983d4419c4
Yitao Chen <ychen404@asu.edu>
2023-11-01 17:03:38 -0700

    sort

--- Project_2/grader_part_3.py
+++ Project_2/grader_part_3.py
@@ -1,41 +1,51 @@
 import json
 
 # Read JSON data from a file
-with open('all_events.json', 'r') as file:
+with open('all_events_new.json', 'r') as file:
     events = json.load(file)
+
+
+# Sort the events by the "logical_clock" attribute
+sorted_events = sorted(events, key=lambda event: event['logical_clock'])
 
 def check_condition(event, customer_requests):
     request_id = event["customer-request-id"]
 
     if request_id not in customer_requests:
-        customer_requests[request_id] = {"last_logical_clock": 0, "propagate_events": []}
+        customer_requests[request_id] = {"branches": {}}
 
     customer_request = customer_requests[request_id]
-    current_clock = event["logical_clock"]
+    branch_id = event["id"]
 
-    # Check if logical clock is incremental within the same request
-    if current_clock < customer_request["last_logical_clock"]:
+    if branch_id not in customer_request["branches"]:
+        customer_request["branches"][branch_id] = {"last_logical_clock": 0}
+
+    current_clock = event["logical_clock"]
+    branch = customer_request["branches"][branch_id]
+
+    # Check if logical clock is incremental within the same request and branch
+    if current_clock <= branch["last_logical_clock"]:
         return False
 
-    customer_request["last_logical_clock"] = current_clock
+    branch["last_logical_clock"] = current_clock
 
     if event["interface"].startswith("propogate"):
         # If the event is a propagate event, store it for later comparison
-        customer_request["propagate_events"].append(event)
+        customer_request["propagate_events"] = event
     else:
         # Check if non-propagate events happen before propagate events
-        for propagate_event in customer_request["propagate_events"]:
-            if current_clock > propagate_event["logical_clock"]:
-                return False
+        if "propagate_events" in customer_request and current_clock > customer_request["propagate_events"]["logical_clock"]:
+            return False
 
     return True
+
 
 customer_requests = {}
 correct_events = 0
 total_events = 0
 incorrect_events = 0
 
-for event in events:
+for event in sorted_events:
     total_events += 1
     if (check_condition(event, customer_requests)):
         correct_events += 1
